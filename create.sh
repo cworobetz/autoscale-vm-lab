70f644f353a009bf8ea38dc2d0e78bbf7119ca38
Cooper Worobetz <-s>
2018-12-17 17:29:26 -0800

    Mon Dec 17 17:29:26 STD 2018
    Signed-off-by: Cooper Worobetz <-s>

--- create.sh
+++ create.sh
@@ -1,3 +1,6 @@
-apt-get install ansible git -y
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+apt-get update
+apt-get install ansible
 ansible-playbook playbooks/setup.yml
 ansible-playbook playbooks/webserver.yml
