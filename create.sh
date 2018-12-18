62f5541063355485ef5775bf5c6f1289e3b6e489
Cooper Worobetz <-s>
2018-12-17 18:50:39 -0800

    Mon Dec 17 18:50:39 STD 2018
    Signed-off-by: Cooper Worobetz <-s>

--- create.sh
+++ create.sh
@@ -2,6 +2,6 @@ sed -i '/ansible/d' /etc/apt/sources.list
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt-get update
-apt-get install ansible -y
+apt-get install ansible -y --allow-unauthenticated
 ansible-playbook playbooks/setup.yml
 ansible-playbook playbooks/webserver.yml
