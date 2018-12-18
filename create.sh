a0bb6cd6972fcad84ac5f7e34b799022a93dbc73
Cooper Worobetz <-s>
2018-12-17 20:17:24 -0800

    .
    Signed-off-by: Cooper Worobetz <-s>

--- create.sh
+++ create.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-apt-get update
-apt-get install ansible -y --allow-unauthenticated
+apt update
+apt install ansible -y --allow-unauthenticated
 ansible-playbook playbooks/setup.yml
 ansible-playbook playbooks/webserver.yml
